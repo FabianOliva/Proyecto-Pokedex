aeab4c3b07f4b546f89152f086462fd7febcaf5c
FabianOliva <fabianoliva01@hotmail.com>
2023-05-23 22:07:53 -0300

    First Push

---
+++ src/components/Main_Card/index.jsx
@@ -0,0 +1,15 @@
+import "./index.css";
+
+const Main_Card = () => {
+  return (
+    <div className="Main_Card_Cont">
+      <span className="Main_Card-id">#304</span>
+      <div className="Main_Card-img_Cont">
+        <img className="Main_Card-img" src="Iconos e imagenes\aron.png" alt="" />
+      </div>
+      <div className="Main_Card-name">Aron</div>
+    </div>
+  );
+};
+
+export default Main_Card;
