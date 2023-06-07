7711feade326c19bea1e0f1d8127835d1798520a
FabianOliva <fabianoliva01@hotmail.com>
2023-06-07 20:26:01 -0300

    Test

--- src/components/Header/sort.jsx
+++ src/components/Header/sort.jsx
@@ -4,18 +4,21 @@ import arrow from "../../../public/Arrow.svg";
 
 const Sort = ({ toggleChange, sortType }) => {
   return (
-    <span className="Header_Sort" onClick={() => toggleChange()}>
-      <div className="Header_Sort_text">
-        {sortType == "#-down" || sortType == "#-up" ? (
-          <span>#</span>
-        ) : (
-          <>
-            <span>A</span>
-            <span>Z</span>
-          </>
-        )}
-      </div>
-      <img src={arrow} alt="Arrow" className={sortType == "#-up" || sortType == "Z-A" ? "arrow-rotate" : ""} />
-    </span>
+    <>
+      <span>En | Es</span>
+      <span className="Header_Sort" onClick={() => toggleChange()}>
+        <div className="Header_Sort_text">
+          {sortType == "#-down" || sortType == "#-up" ? (
+            <span>#</span>
+          ) : (
+            <>
+              <span>A</span>
+              <span>Z</span>
+            </>
+          )}
+        </div>
+        <img src={arrow} alt="Arrow" className={sortType == "#-up" || sortType == "Z-A" ? "arrow-rotate" : ""} />
+      </span>
+    </>
   );
 };
